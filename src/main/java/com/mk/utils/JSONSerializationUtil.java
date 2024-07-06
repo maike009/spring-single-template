91161ac8466d7aac316fa43fff1df6646c83941f
maike001 <maikemk162@gmail.com>
2024-07-06 19:59:08 +0800

    添加了JSONSerializationUtil工具，增强了JwtUtil

---
+++ src/main/java/com/mk/utils/JSONSerializationUtil.java
@@ -0,0 +1,52 @@
+package com.mk.utils;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES;
+
+public class JSONSerializationUtil {
+
+    public static final String DEFAULT_DATE_FORMAT = "yyyy-MM-dd";
+    public static final String DEFAULT_DATE_TIME_FORMAT_FULL = "yyyy-MM-dd HH:mm:ss";
+    public static final String DEFAULT_DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm";
+    public static final String DEFAULT_TIME_FORMAT = "HH:mm:ss";
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    static {
+        configureObjectMapper();
+    }
+
+    private static void configureObjectMapper() {
+        JSONSerializationUtil.objectMapper.configure(FAIL_ON_UNKNOWN_PROPERTIES, false);
+        JSONSerializationUtil.objectMapper.registerModule(new SimpleModule()
+                .addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern(DEFAULT_DATE_TIME_FORMAT)))
+                .addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern(DEFAULT_DATE_TIME_FORMAT_FULL)))
+                .addDeserializer(LocalDate.class, new LocalDateDeserializer(DateTimeFormatter.ofPattern(DEFAULT_DATE_FORMAT)))
+                .addDeserializer(LocalTime.class, new LocalTimeDeserializer(DateTimeFormatter.ofPattern(DEFAULT_TIME_FORMAT)))
+                .addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DEFAULT_DATE_TIME_FORMAT)))
+                .addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DEFAULT_DATE_TIME_FORMAT_FULL)))
+                .addSerializer(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ofPattern(DEFAULT_DATE_FORMAT)))
+                .addSerializer(LocalTime.class, new LocalTimeSerializer(DateTimeFormatter.ofPattern(DEFAULT_TIME_FORMAT))));
+    }
+
+    public static String serialize(Object object) throws JsonProcessingException {
+        return objectMapper.writeValueAsString(object);
+    }
+
+    public static <T> T deserialize(String jsonString, Class<T> clazz) throws JsonProcessingException {
+        return objectMapper.readValue(jsonString, clazz);
+    }
+}
--- src/main/java/com/mk/utils/JwtUtil.java
+++ src/main/java/com/mk/utils/JwtUtil.java
@@ -9,33 +9,43 @@ import java.util.Map;
 
 public class JwtUtil {
 
+    private static final String SIGN_KEY = "security#MK#@!key";
 
     public static String createJwt (String signKey, Long expire, Map<String, Object> claims) {
-        String jwt = Jwts.builder()
+        return Jwts.builder()
                 .signWith(SignatureAlgorithm.HS384, signKey) // 设置签名算法
                 .setClaims(claims) // 自定义内容
                 .setExpiration(new Date(System.currentTimeMillis()+expire))
                 .compact();
-        return jwt;
     }
+
     public static Claims parseJwt(String signKey,String jwt) {
-        Claims claims = Jwts.parser()
+        return Jwts.parser()
                 .setSigningKey(signKey)
                 .parseClaimsJws(jwt)
                 .getBody();
-        return claims;
     }
 
-//    public static void main(String[] args) {
-//        /*IntStream intStream = Stream.of(32, 24, 42, 34, 53, 53, 75, 2, 31, 14, 15, 15)
-//                .mapToInt(u -> u);*/
-//        Stream<String> sorted = Stream.of("apple", "blue", "cherry", "pear", "hello")
-//                .sorted(Comparator.comparing(String::length).reversed());
-//        List.of("A","B","C","D","F","G","H").parallelStream()
-//                .map(String::toLowerCase)
-//                .forEachOrdered(System.out::println);
-//
-//    }
+    /**
+     * jwt重载，默认过期时间为3天，默认key
+     * @return
+     */
+    public static String createJwt ( Map<String, Object> claims) {
+        long expire = 3 * 24 * 60 * 60 * 1000;// 过期时间为3天
+        return Jwts.builder()
+                .signWith(SignatureAlgorithm.HS384, SIGN_KEY) // 设置签名算法
+                .setClaims(claims) // 自定义内容
+                .setExpiration(new Date(System.currentTimeMillis()+expire))
+                .compact();
+
+    }
+
+    public static Claims parseJwt(String jwt) {
+        return Jwts.parser()
+                .setSigningKey(SIGN_KEY)
+                .parseClaimsJws(jwt)
+                .getBody();
+    }
 
 
 }
